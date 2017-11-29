d71b7eee79b15b07dbc0e14e819951ea88a2c08d
Sumant Shiv <sushiv@microsoft.com>
2017-11-29 11:06:58 -0800

    Add extensionProfile to scaleset.

---
+++ sf-2016-unsecure/DeploySFCluster.ps1
@@ -0,0 +1,22 @@
+﻿param
+(
+    [String] $DeploymentNode
+)
+
+Function DeployServiceFabric
+{
+
+    if($env:COMPUTERNAME -eq $DeploymentNode)
+    {
+        $message = "I am the deployment node $DeploymentNode."
+    }
+    else
+    {
+        $message = "I am not the deployment node. It should be $DeploymentNode"
+    }
+
+    $message > "c:\ScriptOutput.txt"
+}
+
+DeployServiceFabric 
+
