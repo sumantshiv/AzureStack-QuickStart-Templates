7bc168943527e8c31212a5d7c32ac8a0321a85fd
Sumant Shiv <sushiv@microsoft.com>
2017-12-08 16:37:32 -0800

    Add PrepareServiceFabric custom script extension for preparing the nodes for deployment.

--- sf-2016-unsecure/DeploySFCluster.ps1
+++ sf-2016-unsecure/DeploySFCluster.ps1
@@ -55,7 +55,8 @@
 
             SetScript = 
             {
-                # Enable File and Printer Sharing for Network Discovery
+                # Enable File and Printer Sharing for Network Discovery (Port 445)
+                Write-Verbose "Opening TCP firewall port 445 for networking."
                 Set-NetFirewallRule -Name 'FPS-SMB-In-TCP' -Enabled True
 
                 # Get the index of current node and match it with the index of required deployment node.
