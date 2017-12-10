c8d0f5812cb68241944e46b72c5cd9c2f274eefd
Sumant Shiv <sushiv@microsoft.com>
2017-12-10 13:20:33 -0800

    Using EndpointSuffix in connection string

--- sf-2016-unsecure/DeploySFCluster.ps1
+++ sf-2016-unsecure/DeploySFCluster.ps1
@@ -146,7 +146,7 @@
 
                 Write-Verbose "Creating diagnostics share at: '$Using:DiagStoreAccountName' blob store"
 
-                $diagStoreConnectinString = "xstore:DefaultEndpointsProtocol=https;AccountName=$Using:DiagStoreAccountName;AccountKey=$Using:DiagStoreAccountKey;BlobEndpoint=local.azurestack.external"
+                $diagStoreConnectinString = "xstore:DefaultEndpointsProtocol=https;AccountName=$Using:DiagStoreAccountName;AccountKey=$Using:DiagStoreAccountKey;EndpointSuffix=local.azurestack.external"
 
                 Write-Verbose "Setting diagnostics store to: '$diagStoreConnectinString'"
                 $configContent.properties.diagnosticsStore.connectionstring = $diagStoreConnectinString
